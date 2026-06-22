f6e62880ed98b556830f60e82fce4aa5c131dfe2
Kartik-Pandey1 <mahiraj974773@gmail.com>
2026-06-22 18:34:25 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://interview-agent-2-fjtz.onrender.com""
+export const ServerUrl  = "https://interview-agent-2-fjtz.onrender.com"
 
 function App() {
 
