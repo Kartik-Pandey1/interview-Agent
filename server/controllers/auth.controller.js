6fa6d6da9ef58bb448614ecf52a2f9607a459987
Kartik-Pandey1 <mahiraj974773@gmail.com>
2026-06-22 18:26:01 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,36 +1,27 @@
-import genToken from "../config/token.js"
-import User from "../models/user.model.js"
-
+import genToken from "../config/token.js";
+import User from "../models/user.model.js";
 
 export const googleAuth = async (req, res) => {
   try {
-    console.log("Body:", req.body);
-
     const { name, email } = req.body;
 
-    console.log("Finding user...");
     let user = await User.findOne({ email });
 
     if (!user) {
-      console.log("Creating user...");
       user = await User.create({
         name,
         email,
       });
     }
 
-    console.log("Generating token...");
     const token = await genToken(user._id);
 
-    console.log("Setting cookie...");
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
       sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-
-    console.log("Login Success");
 
     return res.status(200).json(user);
 
@@ -40,7 +31,28 @@ export const googleAuth = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: error.message,
-      stack: error.stack,
+    });
+  }
+};
+
+export const logOut = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Logout Successfully",
+    });
+  } catch (error) {
+    console.error("Logout Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
     });
   }
 };
