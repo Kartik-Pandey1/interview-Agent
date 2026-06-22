c8d4d6583ee953e7c66f209120417fb54323ebd6
Kartik-Pandey1 <mahiraj974773@gmail.com>
2026-06-22 17:55:18 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,40 +2,46 @@ import genToken from "../config/token.js"
 import User from "../models/user.model.js"
 
 
-export const googleAuth = async (req,res) => {
-    try {
-        const {name , email} = req.body
-        let user = await User.findOne({email})
-        if(!user){
-            user = await User.create({
-                name , 
-                email
-            })
-        }
-        let token = await genToken(user._id)
-        res.cookie("token" , token , {
-            httpOnly:true,
-            secure:true,
-            sameSite:"None",
-            maxAge:7 * 24 * 60 * 60 * 1000
-        })
+export const googleAuth = async (req, res) => {
+  try {
+    console.log("Body:", req.body);
 
-        return res.status(200).json(user)
+    const { name, email } = req.body;
 
+    console.log("Finding user...");
+    let user = await User.findOne({ email });
 
-
-    } catch (error) {
-        return res.status(500).json({message:`Google auth error ${error}`})
+    if (!user) {
+      console.log("Creating user...");
+      user = await User.create({
+        name,
+        email,
+      });
     }
-    
-}
 
-export const logOut = async (req,res) => {
-    try {
-        await res.clearCookie("token")
-        return res.status(200).json({message:"LogOut Successfully"})
-    } catch (error) {
-         return res.status(500).json({message:`Logout error ${error}`})
-    }
-    
+    console.log("Generating token...");
+    const token = await genToken(user._id);
+
+    console.log("Setting cookie...");
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    console.log("Login Success");
+
+    return res.status(200).json(user);
+
+  } catch (error) {
+    console.error("Google Auth Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+      stack: error.stack,
+    });
+  }
+};
 }
